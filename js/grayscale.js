32dbe09a55d97bde51bec84013cfa177b5fa05f4
AdamDonegan <adamjdonegan@gmail.com>
2016-02-14 00:25:08 -0500

    Debugging spotify json response

--- js/grayscale.js
+++ js/grayscale.js
@@ -217,7 +217,7 @@ $(function () {
         , success: function (response) {
           var container = $('span.response');
             if (response) {
-              container.text(response.email);
+              container.text(response.toSource());
             } else {
               alert("An error occurred.");
             }
